de1007e0cd81a9edd7ae6bc20af4279ffb300eb4
yegane alasgarli <63316166+Yegane-Elesgerli@users.noreply.github.com>
2023-01-07 11:47:51 +0400

    editList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
-import Form from "./Feedback/Form.js";
+import React, {useEffect,useState } from 'react'
+import List from './List/List';
+const App=() => {
 
+  const[users,setUsers]=useState([])
 
-
- function App() {
+  useEffect (()=>{
+    fetch('https://5ea5ca472d86f00016b4626d.mockapi.io/brotherhood')
+  .then((res) => res.json())
+  .then((data )=> setUsers(data));
+  },[]);
+  
   return (
-    <div>
-      <Form/>
-    </div>
-  )
-}
+    
+     users.filter((user) => user.id )
+    .map((user)=>(
+      <List 
+  name={user.name}/>
+  
+    ))
+  
+   )
+      }
+export default App
 
-export default App;
+
---
+++ src/List/List.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import "./List.css";
+
+export default class List extends Component {
+
+  render() {
+    const {name}=this.props;
+   
+    return (
+      <div>
+        
+ <p> user name: {name}</p>
+gnfv
+      </div>
+    )
+  }
+}
